7fd601e996120cb2caa7634a6ce46f04c5bb6c1a
wincyysh <141292574+wincyysh@users.noreply.github.com>
2025-10-21 20:19:57 +0200

    Ensure firebaseConfig.js unseen

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, push } from "firebase/database";
+import { getDatabase, ref, set } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
+import { firebaseConfig } from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBnIH3sUKL0U5zWCsyd7X56Hkds6tLO76U",
-  authDomain: "fir-5105b.firebaseapp.com",
-  databaseURL: "https://fir-5105b-default-rtdb.firebaseio.com/",
-  projectId: "fir-5105b",
-  storageBucket: "fir-5105b.firebasestorage.app",
-  messagingSenderId: "764065034501",
-  appId: "1:764065034501:web:74174067eab2c5242fc76c",
-  measurementId: "G-WP1SRHE9Z8"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -31,8 +19,6 @@ function writeUserData(book) {
   .then(()=>console.log("Added new book", book))
   .catch((error)=> console.log("Push failed", error));
 }
-
-
 
 console.log(analytics);
 
